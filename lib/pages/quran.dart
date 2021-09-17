a4dd12d1806acc8612716b3bfa69b88d4fba6d17
taufiqjack <taufiqdwic@gmail.com>
2021-09-17 16:29:17 +0700

    update audio player

--- lib/pages/quran.dart
+++ lib/pages/quran.dart
@@ -1,3 +1,4 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,6 +14,7 @@ class Quran extends StatefulWidget {
 
 class _QuranState extends State<Quran> {
   List? quranList;
+  bool playPause = false;
 
   Future getListQuran() async {
     Response response = await Dio().get('${BaseUrl.listQuran}');
@@ -30,6 +32,17 @@ class _QuranState extends State<Quran> {
 
   @override
   Widget build(BuildContext context) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+
+    void togglePlayPause(x) {
+      setState(() async {
+        playPause = !playPause;
+        try {
+          await assetsAudioPlayer.open(Audio.liveStream('${x['audio']}'));
+        } catch (e) {}
+      });
+    }
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
@@ -53,25 +66,53 @@ class _QuranState extends State<Quran> {
             itemBuilder: (BuildContext context, int index) {
               final x = quranList![index];
               return Card(
-                  child: ListTile(
-                trailing: Text(
-                  '${x['nama']}',
-                  style: GoogleFonts.nunitoSans(
-                      fontSize: 16, fontWeight: FontWeight.bold),
+                child: Column(
+                  children: [
+                    ListTile(
+                      trailing: Text(
+                        '${x['nama']}',
+                        style: GoogleFonts.nunitoSans(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      leading: Text('${x['nomor']}'),
+                      title: Text('Surah : ${x['nama_latin']}'),
+                      subtitle: Text('Jumlah Ayat : ${x['jumlah_ayat']}'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ReadSurah(
+                                      surah: [x],
+                                      index: index,
+                                    )));
+                      },
+                    ),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text('Putar Murrotal'),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        child: playPause
+                            ? Icon(
+                                Icons.stop_circle_outlined,
+                                color: Colors.pink,
+                              )
+                            : Icon(
+                                Icons.play_circle_outline,
+                                color: Colors.pink,
+                              ),
+                        onTap: () async {
+                          togglePlayPause(x);
+                        },
+                      )
+                    ]),
+                    SizedBox(
+                      height: 5,
+                    ),
+                  ],
                 ),
-                leading: Text('${x['nomor']}'),
-                title: Text('Surah : ${x['nama_latin']}'),
-                subtitle: Text('Jumlah Ayat : ${x['jumlah_ayat']}'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ReadSurah(
-                                surah: [x],
-                                index: index,
-                              )));
-                },
-              ));
+              );
             }),
       ),
     );
