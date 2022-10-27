4f87b373b7f643145288e6ac0179e78deb299d25
taufiqjack <taufiqdwic@gmail.com>
2022-10-27 11:55:00 +0700

    update appname

--- lib/pages/doa_view.dart
+++ lib/pages/doa_view.dart
@@ -42,115 +42,110 @@ class _DoaViewState extends State<DoaView> {
 
   @override
   Widget build(BuildContext context) {
-    return BaseView<HomeViewModel>(
-      onModelReady: (data) {
-        data.getDoa(context);
-      },
-      builder: (context, data, child) => Scaffold(
-          appBar: AppBar(
-              backgroundColor: Colors.green.shade800,
-              leading: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MenuHome()));
-                },
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                ),
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.green.shade800,
+            leading: InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MenuHome()));
+              },
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
               ),
-              title: Text(
-                'Doa Sehari -hari',
-              )),
-          body: doaList.isEmpty
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: Container(
-                      child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: doaList.length,
-                    itemBuilder: (context, i) {
-                      var doa = doaList[i];
-                      return Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => DetailDoaView(
-                                          nama: doa.nama,
-                                          lafal: doa.lafal,
-                                          latin: doa.transliterasi,
-                                          arti: doa.arti,
-                                          riwayat: doa.riwayat,
-                                        )));
-                          },
-                          child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Colors.white,
-                                  boxShadow: [
-                                    BoxShadow(
-                                        offset: Offset(2, 2),
-                                        color: Colors.grey.shade200)
-                                  ]),
-                              child: Row(
-                                children: [
-                                  Text('${doa.idDoa}. '),
-                                  SizedBox(
-                                    width: 3,
+            ),
+            title: Text(
+              'Doa Sehari -hari',
+            )),
+        body: doaList.isEmpty
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Container(
+                    child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: doaList.length,
+                  itemBuilder: (context, i) {
+                    var doa = doaList[i];
+                    return Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DetailDoaView(
+                                        nama: doa.nama,
+                                        lafal: doa.lafal,
+                                        latin: doa.transliterasi,
+                                        arti: doa.arti,
+                                        riwayat: doa.riwayat,
+                                      )));
+                        },
+                        child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      offset: Offset(2, 2),
+                                      color: Colors.grey.shade200)
+                                ]),
+                            child: Row(
+                              children: [
+                                Text('${doa.idDoa}. '),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    '${doa.nama}',
+                                    textAlign: TextAlign.start,
                                   ),
-                                  Flexible(
-                                    child: Text(
-                                      '${doa.nama}',
-                                      textAlign: TextAlign.start,
-                                    ),
-                                  ),
-                                ],
-                              )),
-                          //  ListTile(
-                          //   title: Text(
-                          //     '${doa.lafal},',
-                          //     textAlign: TextAlign.end,
-                          //     style: TextStyle(fontSize: 15),
-                          //   ),
-                          //   subtitle: Column(
-                          //       mainAxisAlignment: MainAxisAlignment.start,
-                          //       crossAxisAlignment:
-                          //           CrossAxisAlignment.start,
-                          //       children: [
-                          //         Text(
-                          //           '${doa.transliterasi}',
-                          //           style: TextStyle(
-                          //               fontStyle: FontStyle.italic),
-                          //         ),
-                          //         SizedBox(
-                          //           height: 5,
-                          //         ),
-                          //         Text(
-                          //           'Arti : ${doa.arti}',
-                          //         ),
-                          //         SizedBox(
-                          //           height: 5,
-                          //         ),
-                          //         Text(
-                          //           '(${doa.riwayat})',
-                          //           style: TextStyle(fontSize: 12),
-                          //         )
-                          //       ]),
-                          //   trailing:
-                          //       Text(arabicNumbers.convert('.${i + 1}')),
-                          // ),
-                        ),
-                      );
-                    },
-                  )),
+                                ),
+                              ],
+                            )),
+                        //  ListTile(
+                        //   title: Text(
+                        //     '${doa.lafal},',
+                        //     textAlign: TextAlign.end,
+                        //     style: TextStyle(fontSize: 15),
+                        //   ),
+                        //   subtitle: Column(
+                        //       mainAxisAlignment: MainAxisAlignment.start,
+                        //       crossAxisAlignment:
+                        //           CrossAxisAlignment.start,
+                        //       children: [
+                        //         Text(
+                        //           '${doa.transliterasi}',
+                        //           style: TextStyle(
+                        //               fontStyle: FontStyle.italic),
+                        //         ),
+                        //         SizedBox(
+                        //           height: 5,
+                        //         ),
+                        //         Text(
+                        //           'Arti : ${doa.arti}',
+                        //         ),
+                        //         SizedBox(
+                        //           height: 5,
+                        //         ),
+                        //         Text(
+                        //           '(${doa.riwayat})',
+                        //           style: TextStyle(fontSize: 12),
+                        //         )
+                        //       ]),
+                        //   trailing:
+                        //       Text(arabicNumbers.convert('.${i + 1}')),
+                        // ),
+                      ),
+                    );
+                  },
                 )),
-    );
+              ));
   }
 }
